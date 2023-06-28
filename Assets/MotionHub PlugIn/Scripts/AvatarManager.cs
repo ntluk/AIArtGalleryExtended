ddfdcd7847037c46458c2eb3a12fa105de31b7dd
Julia Lückhof <julia.lueckhof@study.hs-duesseldorf.de>
2023-06-28 13:33:30 +0200

    kinect test

---
+++ Assets/MotionHub PlugIn/Scripts/AvatarManager.cs
@@ -0,0 +1,262 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MMH
+{
+    public class AvatarManager : MonoBehaviour
+    {
+
+        private OSC oscmanager;
+
+        public GameObject avatarPrefab;
+
+        Dictionary<int, GameObject> avatarPool;
+
+        [Range(0.01f, 10.0f)]
+        public float timeout = 0.25f;
+
+        private Camera mainCam;
+
+        public bool debugShowAvatarName = false;
+        public bool debugShowAvatarPosition = false;
+        public bool debugShowAvatar = true;
+        public bool debugShowJoint = false;
+        public bool debugShowJointName = false;
+
+        public Font debugFontLarge, debugFontSmall;
+
+
+        private void Awake()
+        {
+
+
+            mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+            // init avatar pool
+            avatarPool = new Dictionary<int, GameObject>();
+
+        }
+
+        void Start()
+        {
+
+
+            // find oscmanager refference
+            oscmanager = GameObject.Find("$oscmanager").GetComponent<OSC>();
+
+            // set callback function for on receive osc messages
+            oscmanager.SetAddressHandler("/mh/skeleton/", OnReceiveSkeleton);
+
+        }
+
+        public void OnReceiveSkeleton(OscMessage message)
+        {
+
+            // get skeleton id
+            int id = message.GetInt(0);
+
+            // check if avatar is already present
+            GameObject currAvatarObject;
+            Avatar currAvatar;
+            avatarPool.TryGetValue(id, out currAvatarObject);
+
+            if (currAvatarObject != null)
+            {
+
+                // get avatar class refference
+                currAvatar = currAvatarObject.GetComponent<Avatar>();
+
+
+
+                Vector3 globalPositionJoint;
+                Quaternion globalRotationJoint;
+
+                // loop through all joints and update joint pose
+                for (int indexJoint = 0; indexJoint < 21; indexJoint++)
+                {
+
+                    // get global position
+                    globalPositionJoint = new Vector3(
+                                                -message.GetFloat(indexJoint * 8 + 1),
+                                                message.GetFloat(indexJoint * 8 + 2),
+                                                message.GetFloat(indexJoint * 8 + 3)
+                                                     );
+
+                    // get global rotation
+                    globalRotationJoint = new Quaternion(
+                                                    message.GetFloat(indexJoint * 8 + 6),
+                                                    message.GetFloat(indexJoint * 8 + 7),
+                                                    message.GetFloat(indexJoint * 8 + 4),
+                                                    message.GetFloat(indexJoint * 8 + 5)
+                                                        );
+
+                    // set joint pose
+                    currAvatar.setJointPose((Avatar.Joint.JointName)indexJoint, globalPositionJoint, globalRotationJoint);
+
+
+
+                }
+
+
+                // update joint pose
+                currAvatar.updateAvatar();
+
+            }
+            // create a new avatar
+            else
+            {
+
+                // instantiate new avatar and add it to avatar pool
+                currAvatarObject = Instantiate(avatarPrefab, transform);
+                avatarPool.Add(id, currAvatarObject);
+
+                // get avatar class
+                currAvatar = currAvatarObject.GetComponent<Avatar>();
+
+                // set avatar object name and refference in class
+                currAvatarObject.name = "avatar_" + id;
+                currAvatar.setName("avatar_" + id);
+
+                int currTrackerID = (int)Mathf.Floor(id / 1000.0f);
+
+
+                currAvatar.trackerID = currTrackerID;
+
+                currAvatar.skeletonID = id - currTrackerID * 1000;
+
+                Debug.Log("[INFO]: AvatarManager::OnReceiveSkeleton(): Created new avatar with id = " + id);
+
+            }
+        }
+
+        private void Update()
+        {
+
+            Avatar currAvatarClass;
+
+            foreach (KeyValuePair<int, GameObject> currAvatar in avatarPool)
+            {
+
+                currAvatarClass = currAvatar.Value.GetComponent<Avatar>();
+
+                if (currAvatarClass.getTimeLastUpdated() >= timeout)
+                {
+
+                    avatarPool.Remove(currAvatar.Key);
+                    Destroy(currAvatar.Value);
+
+                    Debug.Log("[INFO]: AvatarManager::OnReceiveSkeleton(): Removed avatar with id = " + currAvatar.Key);
+
+                    return;
+
+                }
+                else
+                {
+
+                    currAvatarClass.addTimeLastUpdated();
+
+                }
+            }
+        }
+
+        private void OnGUI()
+        {
+
+            Vector3 screenSpace;
+
+            Dictionary<int, GameObject> avatarPoolCopy = avatarPool;
+
+            foreach (KeyValuePair<int, GameObject> currAvatar in avatarPoolCopy)
+            {
+
+                if (currAvatar.Value != null)
+                {
+
+                    if (debugShowAvatarName)
+                    {
+
+                        GUI.skin.font = debugFontLarge;
+                        GUI.color = Color.black;
+
+                        screenSpace = mainCam.WorldToScreenPoint(currAvatar.Value.GetComponent<Avatar>().getJoint(Avatar.Joint.JointName.HEAD).getTransform().position);
+
+                        if (screenSpace.x > 0 && screenSpace.x < Screen.width && screenSpace.y > 0 && screenSpace.y < Screen.height && screenSpace.z > 0)
+                            GUI.Label(new Rect(screenSpace.x - 32, Screen.height - screenSpace.y - 96, 256, 64), currAvatar.Value.name);
+
+                    }
+
+                    if (debugShowAvatarPosition)
+                    {
+
+                        GUI.skin.font = debugFontSmall;
+                        GUI.color = Color.black;
+
+                        screenSpace = mainCam.WorldToScreenPoint(currAvatar.Value.GetComponent<Avatar>().getJoint(Avatar.Joint.JointName.HEAD).getTransform().position);
+
+                        if (screenSpace.x > 0 && screenSpace.x < Screen.width && screenSpace.y > 0 && screenSpace.y < Screen.height && screenSpace.z > 0)
+                            GUI.Label(new Rect(screenSpace.x - 32, Screen.height - screenSpace.y - 64, 128, 64), currAvatar.Value.GetComponent<Avatar>().getJoint(Avatar.Joint.JointName.HIPS).getTransform().position.ToString());
+
+                    }
+
+                    currAvatar.Value.GetComponent<Avatar>().toggleShowAvatar(debugShowAvatar);
+
+                    currAvatar.Value.GetComponent<Avatar>().toggleDebug(debugShowJoint);
+
+                    if (debugShowJointName)
+                    {
+
+                        GUI.skin.font = debugFontSmall;
+                        GUI.color = Color.gray;
+
+                        foreach (KeyValuePair<Avatar.Joint.JointName, Avatar.Joint> currJoint in currAvatar.Value.GetComponent<Avatar>().getJointPool())
+                        {
+
+                            screenSpace = mainCam.WorldToScreenPoint(currJoint.Value.getTransform().position);
+
+                            if (screenSpace.x > 0 && screenSpace.x < Screen.width && screenSpace.y > 0 && screenSpace.y < Screen.height && screenSpace.z > 0)
+                                GUI.Label(new Rect(screenSpace.x, Screen.height - screenSpace.y, 128, 64), currJoint.Key.ToString());
+
+                        }
+                    }
+                }
+            }
+        }
+
+        public void toggleShowAvatarName(bool value)
+        {
+
+            debugShowAvatarName = value;
+
+        }
+
+        public void toggleShowAvatarPosition(bool value)
+        {
+
+            debugShowAvatarPosition = value;
+
+        }
+
+        public void toggleShowAvatar(bool value)
+        {
+
+            debugShowAvatar = value;
+
+        }
+
+        public void toggleShowJoint(bool value)
+        {
+
+            debugShowJoint = value;
+
+        }
+
+        public void toggleShowJointName(bool value)
+        {
+
+            debugShowJointName = value;
+
+        }
+    }
+
+}
---
+++ Assets/MotionHub PlugIn/Scripts/Point.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using UnityEngine;
+
+namespace MMH {
+    public class Point : Streamable {
+
+        public int pointID;
+
+        public int type;
+        public PointType pointType;
+
+        public Avatar.Joint.JointName jointName;
+
+        public bool Active { get; private set; }
+
+        #region Private Fields
+        int customInt;
+
+        float customFloat;
+
+        GameObject debugMesh;
+
+        GameObject debugSphere;
+        #endregion
+
+        public enum PointType {
+            Undefined,
+            HMD,
+            Controller,
+            Tracker,
+            Camera,
+            Rigidbody,
+            Marker
+        };
+
+        #region MonoBehaviour Callbacks
+        void Awake() {
+            // Init Mesh if configured
+            if (debugMeshAxis != null) {
+                debugMesh = Instantiate(debugMeshAxis, transform);
+            }
+            // Create Primitive if not
+            else { 
+                debugMesh = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                debugMesh.transform.parent = transform;
+                debugMesh.transform.localScale = Vector3.one * 0.075f;
+            }
+
+            debugMesh.transform.localPosition = Vector3.zero;
+            if (debugMaterial != null) {
+                debugMesh.GetComponent<MeshRenderer>().material = debugMaterial;
+            }
+
+            // Create & Init Debug Sphere GameObject
+            debugSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            debugSphere.transform.parent = transform;
+            debugSphere.transform.localScale = Vector3.one * 0.075f;
+            debugSphere.transform.localPosition = Vector3.zero;
+
+            // Assign Deug Material if configured
+            if (debugMaterial != null) {
+                debugSphere.GetComponent<MeshRenderer>().material = debugMaterial;
+            }
+
+            // Disable Debug Sphere
+            debugSphere.SetActive(false);
+        }
+        #endregion
+
+        public void SetPose(Vector3 position, Quaternion rotation) {
+            transform.localPosition = position;
+            transform.localRotation = rotation;
+
+            resetTimeLastUpdated();
+        }
+
+        public void SetType(int type) {
+            this.type = type;
+            pointType = (PointType)type;
+
+            switch (pointType) {
+                case PointType.Marker:
+                    debugMesh.SetActive(false);
+                    debugSphere.SetActive(true);
+                    break;
+                default:
+                    debugMesh.SetActive(true);
+                    debugSphere.SetActive(false);
+                    break;
+            }
+        }
+
+        public bool GetValid() {
+            return Active;
+        }
+
+        public void SetValid(int valid) {
+            Active = (valid == 1 ? true : false);
+
+            if (valid == 1) {
+                SetMeshActive(true);
+            } else {
+                SetMeshActive(false);
+            }
+        }
+
+        public int GetCustomInt() {
+            return customInt;
+        }
+
+        public void SetCustomInt(int customInt) {
+            this.customInt = customInt;
+            jointName = (Avatar.Joint.JointName)customInt;
+        }
+
+        public float GetCustomFloat() {
+            return customFloat;
+        }
+
+        public void SetCustomFloat(float customFloat) {
+            this.customFloat = customFloat;
+        }
+
+        void SetMeshActive(bool active) {
+            if (!active) {
+                debugMesh.SetActive(false);
+                debugSphere.SetActive(false);
+
+                return;
+            }
+
+            switch (pointType) {
+                case PointType.Marker:
+                    debugMesh.SetActive(false);
+                    debugSphere.SetActive(true);
+                    break;
+                default:
+                    debugMesh.SetActive(true);
+                    debugSphere.SetActive(false);
+                    break;
+            }
+        }
+    }
+}
