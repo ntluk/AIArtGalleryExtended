b685f2d38c4315f5c87337518f441ef54393f85d
Julia Lückhof <julia.lueckhof@study.hs-duesseldorf.de>
2023-09-27 18:21:06 +0200

    fixes

--- Assets/Videos/VideoPlay.cs
+++ Assets/Videos/VideoPlay.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cd3ef36430031efb8b0b005be6f36e5a7f35e1b92d6663003b5edb0e6afaba3
-size 5747
+oid sha256:437a8fa66112bd02455c983853feb81413a37a145e4ca39d1f0f58ae0633e88d
+size 7217
